70b03a618a4ca0ffcc078b0e22be99b5a88ddf78
Sandeep Bhutia <sandeepbhutia2011@gmail.com>
2022-10-27 13:25:57 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GraphTraverse from './Graph_Module/Graph_Structure';
+
+function App() {
+  return (
+    <div className="App">
+      <GraphTraverse></GraphTraverse>
+    </div>
+  );
+}
+
+export default App;
